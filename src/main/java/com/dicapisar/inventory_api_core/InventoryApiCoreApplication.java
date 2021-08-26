8c2c2d0746b0f2b000f50134970978ec8759b006
Diego Camilo Pinto Sarmiento <dicapisar@gmail.com>
2021-08-26 10:54:19 -0500

    first commit

---
+++ src/main/java/com/dicapisar/inventory_api_core/InventoryApiCoreApplication.java
@@ -0,0 +1,13 @@
+package com.dicapisar.inventory_api_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryApiCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryApiCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dicapisar/inventory_api_core/InventoryApiCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dicapisar.inventory_api_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryApiCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
