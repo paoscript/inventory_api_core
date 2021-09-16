6aa490bd6ed69bcf3363054e552fb650be899555
Diego Camilo Pinto Sarmiento <dicapisar@gmail.com>
2021-09-16 14:00:59 -0500

    feature/Exceptions_Configurations
    Exception handler implementation

---
+++ src/main/java/com/dicapisar/inventory_api_core/Exeptions/InventoryAPICoreException.java
@@ -0,0 +1,19 @@
+package com.dicapisar.inventory_api_core.Exeptions;
+
+import com.dicapisar.inventory_api_core.dtos.ErrorDTO;
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+
+@Data
+public class InventoryAPICoreException extends Exception{
+    private final ErrorDTO error;
+    private final HttpStatus status;
+
+    public InventoryAPICoreException(String message, HttpStatus status) {
+        this.error = new ErrorDTO();
+        this.error.setMessage(message);
+        this.error.setName(this.getClass().getSimpleName());
+        this.status = status;
+    }
+
+}
